c4bd890df8d6567a54a06b5dcb69723c621de696
郭炯韦 <18530633992@163.com>
2022-07-23 01:20:09 +0800

    feat: 添加getStore方法方便在组件之外使用

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-declare function createStore<T>(store: () => T): {
-    useModel: <K extends keyof T>(key: K) => T[K];
-    StoreProvider: (props: {
-        children: React.ReactNode;
-    }) => React.FunctionComponentElement<React.ProviderProps<T>>;
-};
-export default createStore;
